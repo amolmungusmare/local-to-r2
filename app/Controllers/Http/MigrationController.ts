9f21d0b4d36f214d37112c1bab882998d274e95f
Amol Mungusmare <amol.mungusmare@gmail.com>
2024-05-08 13:20:37 +0530

    update script

--- app/Controllers/Http/MigrationController.ts
+++ app/Controllers/Http/MigrationController.ts
@@ -2,7 +2,6 @@
 import Database from '@ioc:Adonis/Lucid/Database'
 import Drive from '@ioc:Adonis/Core/Drive'
 import fs from 'fs';
-// import sharp from 'sharp';
 
 export default class MigrationController {
 
@@ -12,7 +11,7 @@ export default class MigrationController {
 
         for(let file of files) {
 
-            if(file.url.includes('bmv.52d45400e195803b')) continue
+            if(file.url.includes('https://cdn.bettermindvi')) continue
 
             if(file?.formats) {
 
@@ -24,44 +23,84 @@ export default class MigrationController {
                     continue
                 }
 
-
-                let url = `https://bmv.52d45400e195803bd6da970dc7961d23.r2.cloudflarestorage.com${file.url.split('/uploads')[1]}`
-
-                await Drive.put(url, contents, { disk: 's3' })
+                await Drive.put(file.url.substr(1), contents, { disk: 's3', visibility:"public", contentType: file.mime })
 
                 let formats = {} as any
 
                 for(let key of Object.keys(file.formats)) {
 
-                    let keafdasddasd = fs.readFileSync(`public/${file.formats[key].url}`)
+                    if(key == 'thumbnail') {
 
-                    let keyurl = `https://bmv.52d45400e195803bd6da970dc7961d23.r2.cloudflarestorage.com${file.formats[key].url.split('/uploads')[1]}`
-
-                    await Drive.put(keyurl, keafdasddasd, { disk: 's3'})
-
-                    formats[key] = { ...file.formats[key], url: keyurl }
-
+                        let keafdasddasd
+                        try {
+                            
+                            keafdasddasd = fs.readFileSync(`public/${file.formats[key].url}`)
+                        } catch (error) {
+                            continue
+                        }
+    
+                        await Drive.put(file.formats[key].url.substr(1), keafdasddasd, { disk: 's3', visibility:"public", contentType: file.formats[key].mime })
+    
+                        formats[key] = { ...file.formats[key], url: `https://cdn.bettermindvibes.com${file.formats[key].url}`}
+                    }
                 }
 
-                // console.log({ ...file, provider: 'strapi-provider-cloudflare-r2', url: url, formats: formats })
-
-                await Database.from('files').where('id', file.id).update({ ...file, provider: 'strapi-provider-cloudflare-r2', url: url, formats: JSON.stringify(formats) })
-
+                if(Object.keys(formats).length) await Database.from('files').where('id', file.id).update({ ...file, provider: 'strapi-provider-cloudflare-r2', url: `https://cdn.bettermindvibes.com${file.url}`, formats: JSON.stringify(formats) })
 
             } else {
+                let contents
+                try {
+                    
+                    contents = fs.readFileSync(`public/${file.url}`)
+                } catch (error) {
+                    continue
+                }
 
-                let contents = fs.readFileSync(`public/${file.url}`)
+                await Drive.put(file.url.substr(1), contents, { disk: 's3',visibility:"public", contentType: file.mime })
 
-                let url = `https://bmv.52d45400e195803bd6da970dc7961d23.r2.cloudflarestorage.com${file.url.split('/uploads')[1]}`
-
-                await Drive.put(url, contents, { disk: 's3' })
-
-                await Database.from('files').where('id', file.id).update({ ...file, provider: 'strapi-provider-cloudflare-r2', url: url })
-
-                // console.log({ ...file, provider: 'strapi-provider-cloudflare-r2', url: url })
+                await Database.from('files').where('id', file.id).update({ ...file, provider: 'strapi-provider-cloudflare-r2', url: `https://cdn.bettermindvibes.com${file.url}` })
             }
+            console.log('asd')
         }
 
         console.log('done'); return
+
+    }
+
+    public async delete() {
+
+        let files = await Database.query().from('files').select('*')
+
+        for(let file of files) {
+
+            if(!file?.formats) continue
+
+            let FileFormates = Object.keys(file?.formats)
+
+
+            if(FileFormates.length > 1) {
+
+                for(let key of FileFormates) {
+
+                    if(key != 'thumbnail') continue
+
+                    // fs.readFileSync(`D:/Projects/migratingToS3/public${file.formats[key].url}`)
+
+                    let keafdasddasd
+
+                    try {
+                        keafdasddasd = fs.readFileSync(`public${file.formats[key].url}`)
+                    } catch (error) {
+                        
+                    }
+
+                    keafdasddasd && console.log('asdf', Buffer.byteLength(keafdasddasd))
+    
+                    // if(keafdasddasd) formats[key] = { ...file.formats[key], sizeInBytes: Buffer.byteLength(keafdasddasd) }
+                }
+            }
+
+            // if(Object.keys(formats).length) await Database.from('files').where('id', file.id).update({ ...file, provider: 'strapi-provider-cloudflare-r2', url: file.url, formats: JSON.stringify(formats) })
+        }
     }
 }
